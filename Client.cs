2f565d7e0eb09ca372f4850856b8de88ceb167bd
Richard Schwab <mail@w.tf-w.tf>
2014-09-29 12:34:01 +0200

    Add Newtonsoft Json Copied Server.cs and Client.cs from BomberLib

---
+++ Client.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BomberLib
+{
+    public class Client
+    {
+        // tcp 
+        TcpClient client;
+        NetworkStream clientStream;
+
+
+        /// <summary>
+        ///     Connects to a server.
+        /// </summary>
+        /// <param name="ip">The ip of the server.</param>
+        /// <param name="port">The port of the server.</param>
+        /// <returns>Success of connection.</returns>
+        public Boolean connect(String ip, int port)
+        {
+            this.client = new TcpClient();
+
+            try
+            {
+                // get server & connect
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                this.client.Connect(serverEndPoint);
+
+                // get stream
+                this.clientStream = this.client.GetStream();
+
+                // new thread
+                Thread listener = new Thread(messageListener);
+
+                // start listener
+                listener.Start();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                //Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Disconnect from the Server
+        /// </summary>
+        /// <returns></returns>
+        public Boolean disconnect()
+        {
+            client.Close();
+            return true;
+        }
+
+        /// <summary>
+        ///     Converts the object to JSON and sends it to server
+        /// </summary>
+        /// <param name="data">Data object</param>
+        /// <returns></returns>
+        public Boolean send(Object data)
+        {
+            // convert to string
+            string json = JsonConvert.SerializeObject(data);
+
+            // send to server
+            return this.sendString(json);
+        }
+
+        /// <summary>
+        ///     Sends plain string to server
+        /// </summary>
+        /// <param name="message">The message to send to the server</param>
+        /// <returns></returns>
+        private Boolean sendString(String message)
+        {
+
+            // encode message
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(message);
+
+            // send
+            this.clientStream.Write(buffer, 0, buffer.Length);
+            this.clientStream.Flush();
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Listener for messages from Server.
+        /// </summary>
+        private void messageListener()
+        {
+            byte[] message = new byte[4096];
+
+            // forever...
+            while (true)
+            {
+                int bytesRead = 0;
+                try
+                {
+                    // wait for message
+                    bytesRead = this.clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    // error
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    // disconnect?
+                    break;
+                }
+
+                // read message
+                ASCIIEncoding encoder = new ASCIIEncoding();
+
+                String get = encoder.GetString(message, 0, bytesRead);
+
+                // noooooooot in final version of class please becausea ääääh
+                /*if (get == "server_stop")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Server has stopped");
+                    Console.ResetColor();
+                    Console.WriteLine("Press any key to exit!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                // print to console (debug)
+                Console.WriteLine(get);*/
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BomberLib
+{
+    /// <summary>
+    ///     Server Class for TCP Communication.
+    /// </summary>
+    public class Server
+    {
+        // TCP
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private List<TcpClient> allClients = new List<TcpClient>();
+        private Log log;
+        private Boolean loggingEnabled;
+        private Boolean serveractive;
+
+        /// <summary>
+        ///     Initialize the server with logging
+        /// </summary>
+        /// <param name="thelog"></param>
+        public Server(Log thelog)
+        {
+            log = thelog;
+            loggingEnabled = true;
+        }
+
+        /// <summary>
+        ///     Initialize the server without logging
+        /// </summary>
+        public Server()
+        {
+            loggingEnabled = false;
+        }
+
+        /// <summary>
+        ///     Starts the server
+        /// </summary>
+        /// <param name="port">The port to start the server on.</param>
+        /// <returns>Success of start process.</returns>
+        public Boolean start(int port)
+        {
+            if (!serveractive)
+            {
+                serveractive = true;
+
+                // set up tcp listener
+                this.tcpListener = new TcpListener(IPAddress.Any, port);
+
+                // set up thread & start the server
+                this.listenThread = new Thread(new ThreadStart(ListenForClients));
+                this.listenThread.Start();
+                if (loggingEnabled) log.info("Server started on port " + port);
+                return true;
+            }
+            else
+            {
+                if (loggingEnabled) log.error("Server is already running");
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Stops the Server
+        /// </summary>
+        /// <returns></returns>
+        public Boolean stop()
+        {
+            if (serveractive)
+            {
+                serveractive = false;
+                if (loggingEnabled) log.warn("Server is going to stop");
+                this.tcpListener.Stop();
+                return true;
+            }
+            else
+            {
+                if (loggingEnabled) log.error("Server is not running");
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///     Converts object to string and send to client
+        /// </summary>
+        /// <param name="client">The client to send the object to.</param>
+        /// <param name="data">The object to send.</param>
+        /// <returns></returns>
+        public Boolean send(TcpClient client, Object data)
+        {
+            // convert to json
+            string json = JsonConvert.SerializeObject(data);
+
+            // send to client
+            return this.sendString(client, json);
+        }
+
+        /// <summary>
+        ///     Sends string to a client
+        /// </summary>
+        /// <param name="client">The client to send the message to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <returns>Success of send process.</returns>
+        private Boolean sendString(TcpClient client, String message)
+        {
+            // get client stream
+            NetworkStream clientStream = client.GetStream();
+
+            // encode message
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(message);
+
+            // send to client
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Send data object to all clients
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        /// <returns>Success of send process.</returns>
+        public Boolean sendToAll(String message)
+        {
+            // loop all clients
+            foreach (TcpClient client in this.allClients)
+            {
+                // send message
+                this.send(client, message);
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Send data to all clients except one
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        /// <param name="exception">The client who wont get the message</param>
+        /// <returns>Success of send process.</returns>
+        public Boolean sendToAllExcept(TcpClient exception, String message)
+        {
+            // loop all clients
+            foreach (TcpClient client in this.allClients)
+            {
+                if (client != exception)
+                {
+                    // send message
+                    this.send(client, message);
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Listener for Communication
+        /// </summary>
+        private void ListenForClients()
+        {
+            if(serveractive) this.tcpListener.Start();
+
+            while (serveractive)
+            {
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+
+                //getting error when waiting for client while server is already closed
+                try
+                {
+
+                    //blocks until a client has connected to the server
+                    TcpClient client = this.tcpListener.AcceptTcpClient();
+
+                    //create a thread to handle communication 
+                    //with connected client
+                    clientThread.Start(client);
+                    // second part = IP of client
+                    if (loggingEnabled) log.info("New connection to " + ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+                }
+                catch (Exception e)
+                {
+                    if (loggingEnabled) log.warn("Server stopped");
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Handles client communication.
+        /// </summary>
+        /// <param name="client">Client object</param>
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+
+            // add to client list
+            this.allClients.Add(tcpClient);
+
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    if(loggingEnabled) log.error("a socket error has occurred");
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                String newstring = encoder.GetString(message, 0, bytesRead);
+                Console.WriteLine(newstring);
+                this.sendToAllExcept(tcpClient, newstring);
+            }
+
+            // remove from array
+            this.allClients.Remove(tcpClient);
+
+            tcpClient.Close();
+            if(loggingEnabled) log.info("the client has disconnected from the server");
+        }
+
+    }
+}
